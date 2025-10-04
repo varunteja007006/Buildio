b067dee93b1a0a12eaae342ce7d4a18c8271dd95
varun teja <varunteja007006@gmail.com>
2025-10-04 21:03:42 +0530

    🚀 : starter template added

---
+++ buildio_pro/apps/starter-template/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+
+import { Geist, Geist_Mono } from "next/font/google";
+import { Providers } from "@/components/providers";
+
+import "@workspace/ui/globals.css";
+
+const fontSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
+
+export const metadata: Metadata = {
+  title: "Buildio.pro",
+  description: "Build something epic. Buildio.pro",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="apple-mobile-web-app-title" content="buildio.pro" />
+      </head>
+      <body
+        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
+      >
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
