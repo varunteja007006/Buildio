416e9e95512ef70789d513f7ad8e03345c9978e3
varun teja <varunteja007006@gmail.com>
2025-10-06 21:54:11 +0530

    ❌ : remove the package files

--- buildio_pro/packages/drizzle/src/db/schema/contactUs.ts
+++
@@ -1,8 +0,0 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
-
-export const usersTable = pgTable("contact_us", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  description: varchar({ length: 500 }),
-  email: varchar({ length: 255 }).notNull().unique(),
-});
