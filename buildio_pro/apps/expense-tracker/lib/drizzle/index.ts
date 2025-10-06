0d239ccaab776435e5801a903358cd2a60b9f623
varun teja <varunteja007006@gmail.com>
2025-10-06 22:18:02 +0530

    ⚡ : revert env changes

--- buildio_pro/apps/expense-tracker/lib/drizzle/index.ts
+++ buildio_pro/apps/expense-tracker/lib/drizzle/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./db/schema";
 import "dotenv/config";
 
-const DATABASE_URL = `${process.env.DATABASE_URL ?? ""}${process.env.DATABASE_NAME ?? ""}`;
+const DATABASE_URL = `${process.env.DATABASE_URL ?? ""}`;
 
 if (!DATABASE_URL) {
   throw new Error("Database URL not found");
