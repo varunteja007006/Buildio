15a0df56ef1b63979bbb618bcafe98fe73f8ca26
varun teja <varunteja007006@gmail.com>
2025-10-06 22:16:14 +0530

    ✅: update env

--- buildio_pro/apps/expense-tracker/drizzle.config.ts
+++ buildio_pro/apps/expense-tracker/drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 import "dotenv/config";
 
-const DATABASE_URL = `${process.env.DATABASE_URL}${process.env.DATABASE_NAME}`;
+const DATABASE_URL = `${process.env.DATABASE_URL ?? ""}${process.env.DATABASE_NAME ?? ""}`;
 
 export default defineConfig({
   out: "./drizzle",
