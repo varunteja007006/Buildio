e746bca2c2c516e4023e9e6610ebe18c2ba3d089
varun teja <varunteja007006@gmail.com>
2025-10-13 19:33:52 +0530

    ✅ : update the repo

--- buildio_pro/apps/expense-tracker/app/(auth)/auth/[...auth]/page.tsx
+++ buildio_pro/apps/expense-tracker/app/(auth)/auth/[...auth]/page.tsx
@@ -6,53 +6,6 @@ import { useSession, useSignInSocial } from "@/lib/auth-client"; //import the au
 import { Button } from "@workspace/ui/components/button";
 import { useRouter } from "next/navigation";
 
-// const { data, error } = await authClient.signUp.email(
-//   {
-//     email, // user email address
-//     password, // user password -> min 8 characters by default
-//     name, // user display name
-//     image, // User image URL (optional)
-//     callbackURL: "/dashboard", // A URL to redirect to after the user verifies their email (optional)
-//   },
-//   {
-//     onRequest: (ctx) => {
-//       //show loading
-//     },
-//     onSuccess: (ctx) => {
-//       //redirect to the dashboard or sign in page
-//     },
-//     onError: (ctx) => {
-//       // display the error message
-//       alert(ctx.error.message);
-//     },
-//   }
-// );
-
-// const { data, error } = await authClient.signIn.email(
-//   {
-//     /**
-//      * The user email
-//      */
-//     email,
-//     /**
-//      * The user password
-//      */
-//     password,
-//     /**
-//      * A URL to redirect to after the user verifies their email (optional)
-//      */
-//     callbackURL: "/dashboard",
-//     /**
-//      * remember the user session after the browser is closed.
-//      * @default true
-//      */
-//     rememberMe: false,
-//   },
-//   {
-//     //callbacks
-//   }
-// );
-
 export default function Page() {
   const { data, isPending } = useSession();
   const signIn = useSignInSocial();
