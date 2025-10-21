037424374f703816faed26294f1848332a47202f
varun teja <varunteja007006@gmail.com>
2025-10-21 23:18:25 +0530

    ✅: update

--- buildio_pro/packages/vehicle-management-convex/convex/tasks.tsx
+++ buildio_pro/packages/vehicle-management-convex/convex/tasks.tsx
@@ -3,7 +3,15 @@ import { query } from "./_generated/server";
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    const res = await ctx.db.query("tasks").collect()
+    const res = await ctx.db.query("tasks").collect();
+    return res;
+  },
+});
+
+export const getHello = query({
+  args: {},
+  handler: async (_ctx) => {
+    const res = "Hello";
     return res;
   },
 });
