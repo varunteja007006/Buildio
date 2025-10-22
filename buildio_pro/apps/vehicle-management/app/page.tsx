d1189b07cb6d3be388103f646097a98ba076c0be
varun teja <varunteja007006@gmail.com>
2025-10-22 15:09:44 +0530

    ❌ : remove starter package and app template

--- buildio_pro/apps/vehicle-management/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@workspace/ui/components/button";
-
-export default function Page() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-background">
-      <Button>Hello</Button>
-    </div>
-  );
-}
