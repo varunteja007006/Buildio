134e5d2accae190db818f9fcfc027ec3cf7075bb
varun teja <varunteja007006@gmail.com>
2025-11-13 23:44:15 +0530

    ✅ : update the components and css

--- buildio_pro/apps/expense-tracker/app/(protected)/dashboard/app-sidebar.client.tsx
+++ buildio_pro/apps/expense-tracker/app/(protected)/dashboard/app-sidebar.client.tsx
@@ -3,10 +3,8 @@
 import React from "react";
 
 import {
-	AudioWaveform,
 	BookOpen,
 	Bot,
-	Command,
 	Frame,
 	GalleryVerticalEnd,
 	Map,
--- buildio_pro/apps/expense-tracker/app/(protected)/layout.tsx
+++ buildio_pro/apps/expense-tracker/app/(protected)/layout.tsx
@@ -7,23 +7,26 @@ import {
 
 import { AppSideBarClient } from "./dashboard/app-sidebar.client";
 import { BreadCrumbsClient } from "./dashboard/breadCrumbs.client";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default async function Layout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
-
 	return (
 		<SidebarProvider>
 			<AppSideBarClient />
 			<SidebarInset>
 				<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-					<div className="flex items-center gap-2 px-4">
+					<div className="flex flex-1 items-center gap-2 px-4">
 						<SidebarTrigger className="-ml-1" />
 						<Separator
 							orientation="vertical"
 							className="mr-2 data-[orientation=vertical]:h-4"
 						/>
 						<BreadCrumbsClient />
+					</div>
+					<div className="pr-2">
+						<ModeToggle />
 					</div>
 				</header>
 				<div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
