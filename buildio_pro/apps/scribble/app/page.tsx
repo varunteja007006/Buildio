96d89b3f6d728653968317b29976bbeee4887d4c
varun teja <varunteja007006@gmail.com>
2025-12-03 20:59:02 +0530

    ✅: user registration scribble

--- buildio_pro/apps/scribble/app/page.tsx
+++ buildio_pro/apps/scribble/app/page.tsx
@@ -1,32 +1,39 @@
-import HousieNumGenerator from "@/components/housie-num-generator";
-import { ModeToggle } from "../components/mode-toggle";
-import { Dices } from "lucide-react";
+import { UserRegistration } from "@/components/user-registration";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Pencil } from "lucide-react";
 
 export default function Page() {
-  return (
-    <div className="min-h-screen flex flex-col">
-      {/* Header */}
-      <header className="p-6 flex justify-between items-center">
-        <h1 className="text-2xl flex flex-row gap-2 font-bold tracking-tight">
-          <span>
-            <Dices className="size-8" />
-          </span>{" "}
-          Housie
-        </h1>
-        <nav className="space-x-6">
-          <ModeToggle />
-        </nav>
-      </header>
+	return (
+		<div className="min-h-screen flex flex-col">
+			{/* Header */}
+			<header className="p-6 flex justify-between items-center">
+				<h1 className="text-2xl flex flex-row gap-2 font-bold tracking-tight">
+					<span>
+						<Pencil className="size-8" />
+					</span>{" "}
+					Scribble
+				</h1>
+				<nav className="space-x-6">
+					<ModeToggle />
+				</nav>
+			</header>
 
-      {/* Hero */}
-      <main className="flex-1 flex flex-col justify-center items-center text-center px-4">
-        <HousieNumGenerator />
-      </main>
+			{/* Hero */}
+			<main className="flex-1 flex flex-col justify-center items-center text-center px-4">
+				<div className="grid grid-cols-1 gap-5 lg:grid-cols-2 place-items-center h-[500px]">
+					<div className="p-5">
+						<h1 className="text-primary text-5xl">Welcome to Scribble</h1>
+					</div>
+					<div className="p-5">
+						<UserRegistration />
+					</div>
+				</div>
+			</main>
 
-      {/* Footer */}
-      <footer className="text-center mt-10 text-gray-500 p-4 text-sm border-t border-gray-800">
-        © {new Date().getFullYear()} Buildio.pro — Crafted with ❤️
-      </footer>
-    </div>
-  );
+			{/* Footer */}
+			<footer className="text-center mt-10 text-gray-500 p-4 text-sm border-t border-gray-800">
+				© {new Date().getFullYear()} Buildio.pro — Crafted with ❤️
+			</footer>
+		</div>
+	);
 }
--- buildio_pro/apps/scribble/components/providers.tsx
+++ buildio_pro/apps/scribble/components/providers.tsx
@@ -2,17 +2,21 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ConvexClientProvider } from "./convex-provider";
+import { UserStoreProvider } from "@/lib/store/user.store";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-      enableColorScheme
-    >
-      {children}
-    </NextThemesProvider>
-  );
+	return (
+		<NextThemesProvider
+			attribute="class"
+			defaultTheme="system"
+			enableSystem
+			disableTransitionOnChange
+			enableColorScheme
+		>
+			<ConvexClientProvider>
+				<UserStoreProvider>{children}</UserStoreProvider>
+			</ConvexClientProvider>
+		</NextThemesProvider>
+	);
 }
