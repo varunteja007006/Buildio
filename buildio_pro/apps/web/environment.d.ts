d4695fe615216c90b2e7a1e011515822330a2407
varunteja <varunteja007006@gmail.com>
2026-09-01 22:49:08 +0530

    style(web): apply prettier formatting

--- buildio_pro/apps/web/environment.d.ts
+++ buildio_pro/apps/web/environment.d.ts
@@ -1,6 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     // Next.js
-    NODE_ENV: "development" | "production" | "test"
+    NODE_ENV: "development" | "production" | "test";
   }
 }
